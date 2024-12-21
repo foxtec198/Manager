0aeb94f42baf2665aca6b15f52d0c1fff80a3db1
foxtec198 <foxtec198@gmail.com>
2024-12-21 12:39:39 -0300

    .

--- src/js/manager.js
+++ src/js/manager.js
@@ -5,7 +5,7 @@ var spinner = '<span class="spinner-border spinner-border-sm text-light" role="s
 
 var api = 'https://api.hubbix.com.br'
 // var api = 'https://apihubbix.freeddns.org'
-var api = 'http://127.0.0.1:5432'
+// var api = 'http://127.0.0.1:5432'
 
 var cr = localStorage.getItem('cr')
 var gc = localStorage.getItem('gc')
