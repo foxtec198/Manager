0bff213d7e6441a25353044ff0d431679165e780
foxtec198 <foxtec198@gmail.com>
2024-12-27 14:53:19 -0300

    .

--- src/models/barcd.py
+++
@@ -1,4 +0,0 @@
-from barcode import EAN13
-
-e = EAN13('123123123123')
-e.save('teste')
--- src/models/myde.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-
-def load():
-    with open('.env') as path:
-        for item in path:
-            item = item.split('=')
-            key = item[0]
-            value = item[1].strip('\n')
-            environ[key] = value
