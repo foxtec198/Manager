36d88ea784e593ac3ab46d00855b2012b68e3a05
Guilherme Breve <foxtec198@gmail.com>
2026-04-29 01:09:29 -0300

    Add: Integrate cart functionality and enhance product table management

--- assets/js/config/env.js
+++ assets/js/config/env.js
@@ -7,3 +7,4 @@ export const server = "https://dev.api.hubbix.com.br"
 export const api = server + "/api/manager/";
 export const img = "../assets/img/fav.png"
 export const primary = root_style.getPropertyValue("--primary").trim()
+export const cart = {}
