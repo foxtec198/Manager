b3d98ab8a35a1774679fa7a8b2201f11061a06fa
Guilherme Breve <foxtec198@gmail.com>
2026-06-03 12:15:57 -0300

    Changed const cart for let cart

--- assets/js/config/env.js
+++ assets/js/config/env.js
@@ -3,4 +3,4 @@
 export const server = "https://dev.api.hubbix.com.br"
 export const api = server + "/api/manager/";
 export const img = "../assets/img/fav.png"
-export const cart = {}
+export let cart = {}
